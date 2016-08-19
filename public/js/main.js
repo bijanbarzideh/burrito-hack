b9b9049ed78a026821381bddc42596c9d7b3a478
Student <student@Admins-Mac-mini-57.local>
2016-08-19 16:09:48 -0600

    quickcommit

--- public/js/main.js
+++ public/js/main.js
@@ -2,45 +2,58 @@ angular.module('Burritofy', [])
 	.controller('orderController',
 		['$http',
 		orderController])
-	.controller('burritoController',
-		['burritoFactory',
-		burritoController])
 	.factory('burritoFactory', burritoFactory);
 
 	function orderController ($http) {
 		var oCtrl = this;
 
-		var orders = [];
-
-		function orderCreator (orderInfo){
-			this.firstName = orderInfo.firstName;
-			this.lastName = orderInfo.lastName;
-			this.streetAddress = orderInfo.streetAddress || '';
-			this.city = orderInfo.city || '';
-			this.zipCode = orderInfo.zipCode || '';
-			this.phone = orderInfo.phone;
+		oCtrl.orders = [];
+		console.log('ngcontroller')
+		oCtrl.orderCreator = function(orderInfo){
+			this.firstName = orderInfo.firstName || 'Roger';
+			this.lastName = orderInfo.lastName || 'the Gray';
+			this.streetAddress = orderInfo.streetAddress || '1838 Kickass Cir';
+			this.city = orderInfo.city || 'Babylon';
+			this.zipCode = orderInfo.zipCode || '90210';
+			this.phone = orderInfo.phone || '8675309';
 			this.orderType = orderInfo.orderType;
 
-			orders.push(this);
+			oCtrl.orders.push(this);
+			console.log(oCtrl.orders)
+			// $('#deliveryModal').modal()
+			// $('#burritoPicker').modal()
+
 		}
+		oCtrl.burritoArray = ['The Burrito to Conquer all Burritos', 'Meat Tornado', 'The CrossFit Disaster', 'The Asian Invasion', 'The Patti Mayonnaise']
+		// oCtrl.burritoSelection = [];
+
+			// oCtrl.submitOrder = (burritoInfo) => {
+ 	// 	oCtrl.submitOrder = function(burritoInfo){
+		// 		oCtrl.burritoSelection.push(oCtrl.burritoArray[burritoInfo]);
+		// 		console.log(oCtrl.burritoSelection)
+		// 	}
+
+		// oCtrl.chosenBurrito = "The Chosen Burrito"
+
+		oCtrl.chosenBurrito = localStorage.getItem('burrito') || "The Chosen Burrito"
+
+		oCtrl.submitOrder = function(burritoInfo) {
+			localStorage.setItem('burrito', oCtrl.chosenBurrito = oCtrl.burritoArray[burritoInfo])
+			// oCtrl.chosenBurrito = oCtrl.burritoArray[burritoInfo]
+			return localStorage.getItem('burrito');
+
+
+		}
+
 	}
 
-	function burritoController (burritoFactory) {
-		var bCtrl = this;
 
-		var burritoSelection = [];
-
-			bCtrl.burritoMenu = (bCtrl.burritoChoice) => {
-				burritoSelection.push(burritoArray[burritoChoice]);
-			}
-
-	}
 
 
 	function burritoFactory () {
 		var bFac = this;
 
-		var burritoArray = ['The Burrito to Conquer all Burritos', 'Meat Tornado', 'The CrossFit Disaster', 'The Korean Standard', 'The Patti Mayonnaise']
+
 
 
 		return {
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ var orderCtrl = require("./orders");
 
 module.exports = (app) => {
 	app.get('/', (req,res) => {
-		res.sendFile("home.html", {root: "../public/html"})
+		res.sendFile("home.html", {root: "./public/html"})
 	});
 
+	app.get('/order', (req,res) =>{
+		res.sendFile('order.html', {root: './public/html'})
+	});
+
+	app.get('/order/bselect', (req,res) =>{
+		res.sendFile('bselect.html', {root: './public/html'})
+	})
 
 	app.get("/order", orderCtrl.get);
 	// app.get("/order/:id", orderCtrl.get);
